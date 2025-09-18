e182151d3a4a6ce34f4f67a41c294447fe78098a
j0shtran <hienjoshuatran@gmail.com>
2025-09-18 11:26:26 -0500

    Dockerize C Program

---
+++ Lab 2 Copy/isOdd.c
@@ -0,0 +1,11 @@
+#include "isOdd.h"
+#include <stdio.h>
+
+int isOdd(int num)
+ {
+	if (num % 2 != 0) {
+		printf("This number is odd");
+	}
+	return 0;
+}
+
---
+++ Lab 2 Copy/main.c
@@ -0,0 +1,12 @@
+#include "isOdd.h"
+
+#include <stdio.h>
+
+int main()
+{
+	int myNum;
+	printf("Enter a number: \n");
+	scanf("%d", &myNum);
+	isOdd(myNum);
+	return 0;
+}	
